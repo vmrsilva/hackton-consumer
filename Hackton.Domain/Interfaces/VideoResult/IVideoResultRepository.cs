e9ff3c67fe871bb7a5eba8f3544a0ceae3def804
Valmir <50370623+vmrsilva@users.noreply.github.com>
2025-08-17 22:42:56 -0300

    Implementing saving video processement on mongo.

---
+++ Hackton.Domain/Interfaces/VideoResult/IVideoResultRepository.cs
@@ -0,0 +1,10 @@
+﻿using Hackton.Domain.VideoResult.Entity;
+
+namespace Hackton.Domain.Interfaces.VideoResult
+{
+    public interface IVideoResultRepository
+    {
+        Task<VideoResultEntity> GetByVideoId(Guid VideoId);
+        Task Create(VideoResultEntity videoResultEntity);
+    }
+}
---
+++ Hackton.Domain/VideoResult/Entity/VideoResultEntity.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Hackton.Domain.VideoResult.Entity
+{
+    public class VideoResultEntity
+    {
+        [BsonId]
+        public ObjectId Id { get; } = ObjectId.GenerateNewId();
+        [BsonRepresentation(BsonType.String)]
+        public Guid VideoId { get; set; }
+
+        public IList<ResultItem> Results { get; set; }
+        public DateTime ProcessmentDate { get; } = DateTime.UtcNow;
+
+        public VideoResultEntity(Guid videoId, IList<ResultItem> result)
+        {
+            this.VideoId = videoId;
+            this.Results = result ?? new List<ResultItem>();
+        }
+    }
+
+    public class ResultItem
+    {
+        public TimeSpan Time { get; set; }
+        public string Description { get; set; }
+    }
+}
